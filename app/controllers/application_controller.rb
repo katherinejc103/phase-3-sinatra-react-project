f3950e80cf7335174788c72ee51f4912fff3d22e
katherine <katherinejc103@gmail.com>
2022-11-05 20:39:41 -0400

    doing get and post request to server

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,4 +6,21 @@ class ApplicationController < Sinatra::Base
     { message: "Good luck with your project!" }.to_json
   end
 
+  get "/outfits" do
+    outfits = Outfit.all
+    outfits.to_json
+  end
+
+  post "/outfits" do 
+    outfit = Outfit.create(params)
+    outfit.to_json
+  end
+
+  get '/seasons' do
+    seasons = Seasons.all
+    season.to_json
+  end
+  
 end
+
+
